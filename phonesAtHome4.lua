2b24ed30d23fb8d93d443186bbe07b448e9d5b7e
CommodoreWhite <CommodoreWhite@users.noreply.github.com>
2016-04-27 09:17:20 +0100

    Added files via upload
    Added script description

--- phonesAtHome4.lua
+++ phonesAtHome4.lua
@@ -1,37 +1,43 @@
--- /lua/scripts/phonesAtHome3.lua by commodore white
+--[[ /lua/scripts/phonesAtHome4.lua by commodore white
+
+Maintains:
+    a local list of phones and when they were last seen,
+    a global list of phones, phonesAtHome, and whether or not they are at home, and
+    a global variable, peopleAtHome, if any phone is at home
+--]]
 
 return {
-  active = true,
+  active = false,
 	on = { "phone*", "timer" },
-  data = { phonesAtHome_State = { initial = {} }, phonesAtHome_Time = {initial={}} },
-	execute = function(domoticz, device , eventInfo)
+  data = {  phonesAtHome_Time = {initial={}} },
+	execute = function(domoticz, device , triggerInfo)
  
-    local GRACE = 12*60 -- seconds
+    local GRACE = 12*60 -- seconds how long a phone can be away before its marked as absent
       
 --[[
-    domoticz.data.phonesAtHome_State = {}
     domoticz.data.phonesAtHome_Time = {}
     domoticz.globalData.phonesAtHome = {}
 --]]
 
     -- record phones that get connected
-    if device ~= nil then
-        if (domoticz.data.phonesAtHome_Time[device.name] == nil or os.time() - domoticz.data.phonesAtHome_Time[device.name] > GRACE)
-        and device.state == "On" then
-            domoticz.notify("Look busy!", device.name:sub(6) .. " has returned to the fold")
+    if triggerInfo.type == domoticz.EVENT_TYPE_DEVICE and device.state == "On" then
+        -- notify if phone not listed or has been away for a while
+        if (domoticz.data.phonesAtHome_Time[device.name] == nil or os.time() - domoticz.data.phonesAtHome_Time[device.name] > GRACE) then
+            domoticz.notify("Look busy!", device.name:sub(6) .. " is home")
         end
         domoticz.data.phonesAtHome_Time[device.name] = os.time()
     end
   
-    -- for each phone
+    -- for each phone listed
     local phoneAtHome = false
     for phone,timestamp in pairs(domoticz.data.phonesAtHome_Time) do
     
-        -- keep phone entry fresh
+       -- keep phone entry fresh
        if domoticz.devices[phone].state == "On" then
            domoticz.data.phonesAtHome_Time[phone] = os.time()     
        end
        
+       -- mark stale phones as absent
        local age = os.time() - domoticz.data.phonesAtHome_Time[phone]
        local state = "Off"
        if age < GRACE then
